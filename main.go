371fc4a9e9c10d67a2926a2a28f872b71cd6abf3
Jerry Young <jerryyoung2911@gmail.com>
2022-12-02 09:44:04 +0700

    Riwayat

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	var input, angka1, angka2, hasil int
+	var riwayat []string
 	for input < 9 {
 		fmt.Println("Pilihan menu")
 		fmt.Println("1. Penjumlahan")
@@ -22,20 +23,27 @@ func main() {
 			switch input {
 			case 1:
 				hasil = angka1 + angka2
+				riwayat = append(riwayat, fmt.Sprintf("%d + %d = %+d\n", angka1, angka2, hasil))
 			case 2:
 				hasil = angka1 - angka2
+				riwayat = append(riwayat, fmt.Sprintf("%d - %d = %04d\n", angka1, angka2, hasil))
 			case 3:
 				hasil = angka1 * angka2
+				riwayat = append(riwayat, fmt.Sprintf("%d * %d = %o\n", angka1, angka2, hasil))
 			case 4:
-				if angka2 > 0 {
+				if angka2 != 0 {
 					hasil = angka1 / angka2
 				} else {
 					hasil = 0
 				}
+
+				riwayat = append(riwayat, fmt.Sprintf("%d / %d = %b\n", angka1, angka2, hasil))
 			}
 			fmt.Println("Hasil :", hasil)
 		} else if input == 5 {
-
+			for i := 0; i < len(riwayat); i++ {
+				fmt.Print(riwayat[i])
+			}
 		}
 	}
 }
